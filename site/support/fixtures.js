2c3113139eba669ab63f570860d53266540fecca
Sam Richardson <sam@richardson.co.nz>
2020-03-30 20:38:37 +1100

    Added fixtures for tests

---
+++ site/support/fixtures.js
@@ -0,0 +1,31 @@
+// Products as returned by Shopify
+export const shopifyProducts = [
+  {
+    description: "description 1",
+    id: "id 1",
+    images: [{ src: "src 1" }],
+    title: "title 1",
+  },
+  {
+    description: "description 2",
+    id: "id 2",
+    images: [{ src: "src 2" }],
+    title: "title 2",
+  },
+];
+
+// Products as given to frontend (transformed from shopifyProducts above)
+export const products = [
+  {
+    description: "description 1",
+    id: "id 1",
+    src: "src 1",
+    title: "title 1",
+  },
+  {
+    description: "description 2",
+    id: "id 2",
+    src: "src 2",
+    title: "title 2",
+  },
+];
