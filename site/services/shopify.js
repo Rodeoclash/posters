7b3efaa33fe8c4946257a712650ff93d6f6569dc
Sam Richardson <sam@richardson.co.nz>
2020-03-30 20:41:31 +1100

    Restore handle behaviour

--- site/services/shopify.js
+++ site/services/shopify.js
@@ -16,7 +16,7 @@ export const buildClient = () => {
  * Given a product, transform it into an object that can be serialised.
  */
 export const serialiseProduct = (product) => ({
-  ...pick(["description", "id", "title"])(product),
+  ...pick(["description", "handle", "id", "title"])(product),
   src: product.images[0].src,
 });
 
