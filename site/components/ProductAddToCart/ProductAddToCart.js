4e08d35588735ae1cadff140977b6236d5516807
Sam Richardson <sam@richardson.co.nz>
2020-04-09 22:53:33 +1000

    More design

--- site/components/ProductAddToCart/ProductAddToCart.js
+++ site/components/ProductAddToCart/ProductAddToCart.js
@@ -5,14 +5,7 @@ import CartContext from "../../contexts/cart";
 const ProductAddToCart = ({ product }) => {
   const { addProduct } = useContext(CartContext);
 
-  return (
-    <button
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={addProduct.bind(this, product)}
-    >
-      Add to cart
-    </button>
-  );
+  return <button onClick={addProduct.bind(this, product)}>Add to cart</button>;
 };
 
 export default ProductAddToCart;
