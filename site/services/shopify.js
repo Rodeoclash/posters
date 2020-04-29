1b636bad09bbc7aac47c6d15d05150b7dc0f3600
Sam Richardson <sam@richardson.co.nz>
2020-04-29 21:34:52 +1000

    Use optimised build

--- site/services/shopify.js
+++ site/services/shopify.js
@@ -1,4 +1,4 @@
-import Client from "shopify-buy/index.unoptimized.umd";
+import Client from "shopify-buy";
 
 const domain = "waitawa-posters.myshopify.com";
 const storefrontAccessToken = "7b2d5b130d19dd191f4c6b61e5a2b244";
