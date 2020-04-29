0441a18917be2f5ae8697f4ff90c1df020a7338a
Sam Richardson <sam@richardson.co.nz>
2020-04-29 21:39:34 +1000

    Fix path

--- site/components/ProductLink/ProductLink.js
+++ site/components/ProductLink/ProductLink.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const ProductLink = ({ product, children }) => (
-  <Link href="/posters/[slug]" as={`/posters/${product.handle}`}>
+  <Link href="/posters/[handle]" as={`/posters/${product.handle}`}>
     <a>{children}</a>
   </Link>
 );
